f987b09c6a7089092a277b1edc314f1c148bfae3
Akhilendra Gadde <akhilendragadde17@gmail.com>
2023-12-28 23:35:59 +0530

    pipeline setup

--- kaggle-survey-2018/main.py
+++ kaggle-survey-2018/main.py
@@ -5,15 +5,18 @@
 import numpy as np
 import pandas as pd
 
+from feature_engine import encoding, imputation
+from sklearn import base, pipeline
+from sklearn import model_selection
+
 
 def tweak_dataset(raw: pd.DataFrame) -> pd.DataFrame:
-    
     """
     Preprocess pipeline for kaggle-survey-2018
     :param raw: Input DataFrame
     :return data: Outputs DataFrame
     """
-    
+
     columns = {
         'Q1': 'gender',
         'Q2': 'age',
@@ -29,15 +32,19 @@ def tweak_dataset(raw: pd.DataFrame) -> pd.DataFrame:
         # 'Q17': 'pref_lang',    
         'Q6': 'target'
     }
-    raw = raw.iloc[1:].filter(columns).rename(columns=columns).reset_index(drop=True)
-    
+    raw: pd.DataFrame = raw.iloc[1:].filter(columns).rename(columns=columns).reset_index(drop=True)
+
     raw.gender = raw.gender.apply(str.lower)
-    raw.age = raw.age.apply(lambda x: x.replace("+", '').split('-')[0]).astype(int)
-    raw.years_exp = raw.years_exp.fillna('0').apply(lambda x: x.replace("+", '').split('-')[0]).astype(float)
+    raw.age = raw.age.apply(lambda x: x.replace("+", '').split('-')[0]).astype(np.int64)
+
+    raw.years_exp = raw.years_exp.fillna('0').apply(lambda x: x.replace("+", '').split('-')[0])
+    raw.years_exp = np.where(raw.years_exp == 0, np.nan, raw.years_exp)
+    raw.years_exp = raw.years_exp.astype(np.int64)
+
     raw.python = raw.python.fillna(0).replace("Python", 1).astype(int)
     raw.r = raw.r.fillna(0).replace("R", 1).astype(int)
     raw.sql = raw.sql.fillna(0).replace("SQL", 1).astype(int)
-    
+
     raw.compensation = raw \
         .compensation \
         .fillna('0') \
@@ -84,8 +91,9 @@ def tweak_dataset(raw: pd.DataFrame) -> pd.DataFrame:
         None,
         '5'
     ]
-    raw.education = np.select(edu_conditions, edu_values, None)
-    raw.education = raw.education.fillna(0).astype(int)
+    raw.education = np.select(edu_conditions, edu_values, 0)
+    raw.education = np.where(raw.education == 0, np.nan, raw.years_exp)
+    raw.education = raw.education.astype(np.int64)
 
     # ML Used
     ml_conditions = [
@@ -107,16 +115,76 @@ def tweak_dataset(raw: pd.DataFrame) -> pd.DataFrame:
     raw.ml_used = np.select(ml_conditions, ml_values, None)
     raw.ml_used = raw.ml_used.fillna('No').astype(object)
 
-    data = raw \
-        .query("country.isin(['usa', 'india']) and target.isin(['Data Scientist', 'Software Engineer'])") \
+    return raw
+
+
+class Transformer(base.BaseEstimator, base.TransformerMixin):
+    """
+    Transformer class - data pipeline cleaner
+    We need to implement fit & transform for extended classes
+
+    Parameters
+    ----------
+    y_col: str, optional
+        Name of the column to be used as target variable.
+
+    Attributes
+    ----------
+    y_col: str, optional
+        Name of the column to be used as target variable.
+    """
+
+    def __init__(self, y_col: str = None):
+        self.y_col = y_col
+
+    def transform(self, X):
+        return tweak_dataset(X)
+
+    def fit(self, X, y=None):
+        return self
+
+
+def get_features_and_labels(df: pd.DataFrame, y_labels: list[str]) -> (pd.DataFrame, pd.DataFrame):
+
+    """
+    Separate Features and Labels from raw dataset prior to preprocessing
+    :param df:
+    :param y_labels:
+    :return: X, y
+    """
+
+    data: pd.DataFrame = df \
+        .query("Q3.isin(['United States of America', 'India']) and Q6.isin(['Data Scientist', 'Software Engineer'])") \
         .reset_index(drop=True).copy(deep=True)
 
-    # print(data.head())
-    return data
+    return data.drop(columns=y_labels), data[y_labels]
+
+
+def setup_pipeline() -> None:
+
+    file: str = r'datasets/multipleChoiceResponses.csv'
+    df: pd.DataFrame = pd.read_csv(file, low_memory=False)
+
+    # tweak_dataset(df).to_pickle(r'datasets/output.p.gz', compression='gzip')
+
+    category: list[str] = ["gender", "country", "major", "ml_used"]
+    impute: list[str] = ["age", "education", "years_exp"]
+    pipe: pipeline.Pipeline = pipeline.Pipeline(
+        [
+            ('transform', Transformer()),
+            ('categorical', encoding.OneHotEncoder(top_categories=5, drop_last=True, variables=category)),
+            ('int_impute', imputation.MeanMedianImputer(imputation_method='median', variables=impute))
+        ]
+    )
+
+    X, y = get_features_and_labels(df, ['Q6'])
+    X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=.3, random_state=42, stratify=y)
+
+    X_train = pipe.fit_transform(X_train, y_train)
+    X_test = pipe.transform(X_test)
+
+    print(X_test)
 
 
 if __name__ == '__main__':
-
-    file = r'datasets/multipleChoiceResponses.csv'
-    df = pd.read_csv(file, low_memory=False)
-    tweak_dataset(df).to_pickle(r'datasets/output.p.gz', compression='gzip')
+    setup_pipeline()
